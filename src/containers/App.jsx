d90f5a20d36e3e112e5a749c5acf9d9e1e81eb33
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-01-30 12:52:30 -0500

    Despliegue de los datos obtenidos por la fake API en los componentes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,30 +7,91 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Categories title='Originales'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const API = 'http://localhost:3000/initalState';
+  const [videos, setVideos] = useState({mylist: [], trends: [], originals: []});
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  console.log(videos);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {videos.mylist.map(({ id, slug, title, type, language, year, contentRating, duration, cover, description, source }) => {
+              return (
+                <CarouselItem
+                  key={id}
+                  slug={slug}
+                  title={title}
+                  type={type}
+                  language={language}
+                  year={year}
+                  contentRating={contentRating}
+                  duration={duration}
+                  cover={cover}
+                  description={description}
+                  source={source}
+                />
+              );
+            })}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.trends.length > 0 && (
+        <Categories title='Tendencias'>
+          <Carousel>
+            {videos.trends.map(({ id, slug, title, type, language, year, contentRating, duration, cover, description, source }) => {
+              return (
+                <CarouselItem
+                  key={id}
+                  slug={slug}
+                  title={title}
+                  type={type}
+                  language={language}
+                  year={year}
+                  contentRating={contentRating}
+                  duration={duration}
+                  cover={cover}
+                  description={description}
+                  source={source}
+                />
+              );
+            })}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.originals.length > 0 && (
+        <Categories title='Originales'>
+          <Carousel>
+            {videos.originals.map(({ id, slug, title, type, language, year, contentRating, duration, cover, description, source }) => {
+              return (
+                <CarouselItem
+                  key={id}
+                  slug={slug}
+                  title={title}
+                  type={type}
+                  language={language}
+                  year={year}
+                  contentRating={contentRating}
+                  duration={duration}
+                  cover={cover}
+                  description={description}
+                  source={source}
+                />
+              );
+            })}
+          </Carousel>
+        </Categories>
+      )}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
