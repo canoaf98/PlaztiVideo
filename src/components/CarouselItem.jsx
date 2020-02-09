90858ec21127e75463a178cb9c881e9b5750ad50
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-02-08 23:05:57 -0500

    Actualización con el uso de redux y funcionalidad de favoritos

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setFavorite, deleteFavorite } from '../actions';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 import '../assets/styles/components/CarouselItem.scss';
 
-const CarouselItem = ({ title, year, contentRating, duration, cover }) => (
-  <div className='carousel-item'>
-    <img className='carousel-item__img' src={cover} alt={title} />
-    <div className='carousel-item__details'>
-      <div>
-        <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
-        <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' />
+const CarouselItem = (props) => {
+  const { id, title, year, contentRating, duration, cover, isList } = props;
+  const handleSetFavorite = () => {
+    props.setFavorite({
+      id, title, year, contentRating, duration, cover,
+    });
+  };
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId);
+  };
+  return (
+    <div className='carousel-item'>
+      <img className='carousel-item__img' src={cover} alt={title} />
+      <div className='carousel-item__details'>
+        <div>
+          <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
+          {isList ? (
+            <img
+              className='carousel-item__details--img'
+              src={removeIcon}
+              alt='Remove Icon'
+              onClick={() => handleDeleteFavorite(id)}
+            />
+          ) : (
+            <img
+              className='carousel-item__details--img'
+              src={plusIcon}
+              alt='Plus Icon'
+              onClick={handleSetFavorite}
+            />
+          )}
+        </div>
+        <p className='carousel-item__details--title'>{title}</p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRating} ${duration}`}
+        </p>
       </div>
-      <p className='carousel-item__details--title'>{title}</p>
-      <p className='carousel-item__details--subtitle'>
-        {`${year} ${contentRating} ${duration}`}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 CarouselItem.propTypes = {
   cover: PropTypes.string,
@@ -28,4 +56,9 @@ CarouselItem.propTypes = {
   duration: PropTypes.number,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+  setFavorite,
+  deleteFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
