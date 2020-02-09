0f6eac13873c072e8bb140274943418c1de29c23
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-02-09 16:38:13 -0500

    Función para obtener la fuente del vídeo que se quiere reproducir

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource} from '../actions';
+import NotFound from './NotFound';
+import '../assets/styles/components/Player.scss';
 
-const Player = () => {
-  return (
+const Player = (props) => {
+  const { history, playing, getVideoSource } = props;
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(playing).length > 0;
+
+  useEffect(() => {
+    getVideoSource(id);
+  }, []);
+  const handleBack = () => {
+    history.goBack();
+  };
+  return hasPlaying ? (
     <div clasName='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
-        <button type='button'>
+        <button type='button' onClick={handleBack}>
           Regresar
         </button>
       </div>
     </div>
-  );
+  ) : <NotFound />;
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
